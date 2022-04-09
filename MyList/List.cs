d5f37ba9d8b66860fabdeaee43000b52db1f12f2
juliion <juliaionova111@gmail.com>
2022-04-09 19:50:20 +0300

    Add list class.
    Create a class definition for a doubly linked list.

---
+++ MyList/List.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MyList
+{
+    public class List
+    {
+        public Node Head { get; private set; }
+        public Node Tail { get; private set; }
+        public int Count { get; private set; }
+    }
+}
