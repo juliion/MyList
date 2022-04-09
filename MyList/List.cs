dcb8972dd892340762a0e2e6dbb584ec73f7174f
juliion <juliaionova111@gmail.com>
2022-04-09 22:38:40 +0300

    Add method InsertAt().
    Add a method to insert an element into the list at a given position.

--- MyList/List.cs
+++ MyList/List.cs
@@ -23,5 +23,50 @@ namespace MyList
 
             Count++;
         }
+        public void InsertAt(char val, int ind)
+        {
+            if (ind == Count + 1)
+                AddLast(val);
+            else if (ind == 0)
+            {
+                var tmp = new Node()
+                { 
+                    Data = val
+                };
+                tmp.Next = Head;
+
+                if (Count != 0)
+                    Head.Prev = tmp;
+
+                if (Count == 0)
+                    Head = Tail = tmp;
+                else
+                    Head = tmp;
+
+                Count++;
+            }
+            else
+            {
+                int i = 0;
+                var ins = Head;
+                while (i < ind)
+                {
+                    ins = ins.Next;
+                    i++;
+                }
+                var prevIns = ins.Prev;
+                var tmp = new Node()
+                {
+                    Data = val
+                };
+                if (prevIns != null && Count != 1)
+                    prevIns.Next = tmp;
+                tmp.Next = ins;
+                tmp.Prev = prevIns;
+                ins.Prev = tmp;
+
+                Count++;
+            }
+        }
     }
 }
