94ef959d848b6eaaa64fe8f7b9d99d2360db206c
juliion <juliaionova111@gmail.com>
2022-04-09 20:04:50 +0300

    Add method AddLast().
    Add a method to add an element to the end of the list.

--- MyList/List.cs
+++ MyList/List.cs
@@ -6,5 +6,22 @@ namespace MyList
         public Node Head { get; private set; }
         public Node Tail { get; private set; }
         public int Count { get; private set; }
+        public void AddLast(char val)
+        {
+            var tmp = new Node() 
+            {
+                Data = val 
+            };
+            if (Count == 0)
+                Head = Tail = tmp;
+            else
+            {
+                Tail.Next = tmp;
+                tmp.Prev = Tail;
+                Tail = tmp;
+            }
+
+            Count++;
+        }
     }
 }
