17f36da325a1babeaff339b089add0bebde675a3
juliion <juliaionova111@gmail.com>
2022-04-09 21:55:11 +0300

    Add a test method that checks the length of an empty list.
    Method that checks that the length of an empty list is zero.

--- MyListTests/ListTests.cs
+++ MyListTests/ListTests.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyList;
 
 namespace MyListTests
 {
     [TestClass]
     public class ListTests
     {
+        [TestMethod]
+        public void Count_EmptyListLength_0()
+        {
+            List emptyList = new List();
+            int expectedLength = 0;
+            int actualLength = emptyList.Count;
+            Assert.AreEqual(expectedLength, actualLength);
+        }
     }
 }
