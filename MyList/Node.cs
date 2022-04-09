2427597570bb40b7492b8f47a2ec0b13a45ad1af
juliion <juliaionova111@gmail.com>
2022-04-09 19:40:07 +0300

    Add node class.
    Сreate a node class definition for a doubly linked list containing data, pointers to the previous and next elements.

---
+++ MyList/Node.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MyList
+{
+    public class Node
+    {
+        public char Data { get; set; }
+        public Node Next { get; set; }
+        public Node Prev { get; set; }
+    }
+}
